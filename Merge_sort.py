99fc42fac6055a375e3b8abdaca6887a0fc2e222
Sahan2 <mehmetsahan77406@gmail.com>
2022-12-14 21:13:20 -0600

    Merge sort

--- Merge_sort.py
+++ Merge_sort.py
@@ -1,80 +1,67 @@
-# Python program for implementation of MergeSort
+# An implementation of MergeSort
+def merge_sorting(array1):
 
-# Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
+    # Checking if the length of array greater than 1
 
+    if len(array1) > 1:
 
-def merge(arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r - m
+        # Finding the mid of the array
 
-    # create temp arrays
-    L = [0] * (n1)
-    R = [0] * (n2)
+        mid_array = len(array1)//2
 
-    # Copy data to temp arrays L[] and R[]
-    for i in range(0, n1):
-        L[i] = arr[l + i]
+    # Dividing the array elements
 
-    for j in range(0, n2):
-        R[j] = arr[m + 1 + j]
+        left_array = array1[:mid_array]
 
-    # Merge the temp arrays back into arr[l..r]
-    i = 0	 # Initial index of first subarray
-    j = 0	 # Initial index of second subarray
-    k = l	 # Initial index of merged subarray
+        right_array = array1[mid_array:]
 
-    while i < n1 and j < n2:
-        if L[i] <= R[j]:
-            arr[k] = L[i]
-            i += 1
-        else:
-            arr[k] = R[j]
-            j += 1
-        k += 1
+    # Sorting the both halves
 
-    # Copy the remaining elements of L[], if there
-    # are any
-    while i < n1:
-        arr[k] = L[i]
-        i += 1
-        k += 1
+        merge_sorting(left_array)
 
-    # Copy the remaining elements of R[], if there
-    # are any
-    while j < n2:
-        arr[k] = R[j]
-        j += 1
-        k += 1
+        merge_sorting(right_array)
 
-# l is for left index and r is right index of the
-# sub-array of arr to be sorted
+        a = b = c = 0
 
+    # Copy data to temp arrays left_array[] and right_array[]
 
-def mergeSort(arr, l, r):
-    if l < r:
+        while a < len(left_array) and b < len(right_array):
+            if left_array[a] <= right_array[b]:
+                array1[c] = left_array[a]
+                a += 1
+            else:
+                array1[c] = right_array[b]
+                b += 1
+            c += 1
 
-        # Same as (l+r)//2, but avoids overflow for
-        # large l and h
-        m = l+(r-l)//2
+    # Checking if any element was left
 
-        # Sort first and second halves
-        mergeSort(arr, l, m)
-        mergeSort(arr, m+1, r)
-        merge(arr, l, m, r)
+        while a < len(left_array):
+
+            array1[c] = left_array[a]
+            a += 1
+            c += 1
+
+        while b < len(right_array):
+
+            array1[c] = right_array[b]
+            b += 1
+            c += 1
+
+    # print the array
 
 
-# Driver code to test above
-arr = [12, 11, 13, 5, 6, 7]
-n = len(arr)
-print("Given array is")
-for i in range(n):
-    print("%d" % arr[i], end=" ")
+def print_array(array):
 
-mergeSort(arr, 0, n-1)
-print("\n\nSorted array is")
-for i in range(n):
-    print("%d" % arr[i], end=" ")
+    for i in range(len(array)):
+        print(array[i], end=" ")
+    print()
 
-# This code is contributed by Mohit Kumra
+
+if __name__ == '__main__':
+
+    array = [90, -1, 17, 4, 6, 101, -2]
+
+    print_array(array)
+    merge_sorting(array)
+    print_array(array)
