56facc4979abed0232da2aa2c973bc81abcf8140
Sahan2 <mehmetsahan77406@gmail.com>
2022-12-16 21:56:01 -0600

    Added new jira reporting file

--- jira_reporting.py
+++ jira_reporting.py
@@ -1,11 +1,11 @@
 
 from jira import JIRA
 import sys
-import maskpass  
+import maskpass
 import base64
-  
-jiraOptions = {'server': "https://jiradc.ext.net.nokia.com/"}
-  
+
+jiraOptions = {'server': "https://xxxxxxxxxxxxxx.com/"}
+
 username = input("Enter username: ")
 password = maskpass.askpass("Enter password : ")
 encpwd = base64.b64encode(password.encode("utf-8"))
@@ -13,19 +13,16 @@ date = input("Enter a date formatted as YYYY-MM-DD: ")
 
 jira = JIRA(options=jiraOptions, basic_auth=(
     username, password))
-  
-file_path = 'Jira Report 1.txt'
-File =sys.stdout = open(file_path, "w")
 
-for singleIssue in jira.search_issues(jql_str='project = CISAMTSTAUTO AND created>=' + date): 
+file_path = 'Jira Report 1.txt'
+File = sys.stdout = open(file_path, "w")
+
+for singleIssue in jira.search_issues(jql_str='project = CISAMTSTAUTO AND created>=' + date):
     print('{}{}{}{}{}{}'.format(50*'=', '\nKey: ' + singleIssue.key, '\nSummary: ' + singleIssue.fields.summary,
-                             '\nReporter: ' + singleIssue.fields.reporter.displayName, '\nStatus: ' + singleIssue.fields.status.name,
-                             '\nUpdate Date: ' + singleIssue.fields.updated[0:10]))
+                                '\nReporter: ' + singleIssue.fields.reporter.displayName, '\nStatus: ' +
+                                singleIssue.fields.status.name,
+                                '\nUpdate Date: ' + singleIssue.fields.updated[0:10]))
     if (singleIssue.fields.assignee is None):
-        print('Assignee: None' )
+        print('Assignee: None')
     else:
-        print('Assignee: ' + singleIssue.fields.assignee.displayName)   
-            
-            
-            
-    
+        print('Assignee: ' + singleIssue.fields.assignee.displayName)
